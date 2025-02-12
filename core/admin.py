9b200e0c648105efc6d033ea2366a6e3530acc6c
giru <yagofarov.vadim@mail.ru>
2025-02-13 00:00:27 +0500

    add admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core.models import User
+
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'username')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    exclude = ('password',)
+    readonly_fields = ('last_login', 'date_joined')
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        if not obj:
+            return fieldsets
+        return [(name, {'fields': [f for f in fields if f != 'password']}) for name, fields in fieldsets]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- todolist/settings.py
+++ todolist/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '542525Zz',
+        'HOST': 'localhost',
+        'PORT': 5433,
+        'TEST': {
+               'NAME': 'test_db',  # Укажите имя тестовой базы данных
+           },
     }
 }
 
